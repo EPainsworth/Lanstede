1c524a700303e43bf700043eca2d1606df2f097a
MarijnGroen <56343501+MarijnGroen@users.noreply.github.com>
2021-04-05 13:19:25 +0200

    elias nee

--- resources/views/widgets/PopularGames.blade.php
+++ resources/views/widgets/PopularGames.blade.php
@@ -1,17 +1,17 @@
     <div class="borderStyle1">
         <div class="picture1">
-            <img src="{{asset('assets/csgo_thumbnail.png')}}" alt="Error loading image">
+            <img src="https://gyazo.com/534657989ef9b241ba6895cfbcbfd572.png" alt="Error loading image">
         </div>
     </div>
 
     <div class="borderStyle2">
         <div class="picture2">
-            <img src="{{asset('assets/csgo_thumbnail.png')}}" alt="Error loading image">
+            <img src="https://gyazo.com/2b58b2a78b2ac946c2e971b1bd0552c0.png" alt="Error loading image">
         </div>
     </div>
 
     <div class="borderStyle3">
         <div class="picture3">
-            <img src="{{asset('assets/csgo_thumbnail.png')}}" alt="Error loading image">
+            <img src="https://gyazo.com/274b43d2b91cb37660829ae93741674c.png" alt="Error loading image">
         </div>
     </div>
